e5abc80a1ea80c70c8e366aefd4d33080ce40a14
Roman Gershman <romange@gmail.com>
2018-01-03 17:34:04 +0200

    Add async reads to vol print

--- puma/file/vol_print.cc
+++ puma/file/vol_print.cc
@@ -2,6 +2,9 @@
 // Author: Roman Gershman (romange@gmail.com)
 //
 
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 #include "base/init.h"
@@ -11,10 +14,13 @@
 
 #include "strings/numbers.h"
 #include "strings/util.h"
+#include "util/sq_threadpool.h"
 
 DEFINE_string(table, "", "table name");
 DEFINE_bool(skip_print, false, "");
 DEFINE_bool(bin, false, "");
+DEFINE_int32(threads, 0, "Async");
+DEFINE_int32(io_len, 4, "");
 
 using namespace puma;
 using namespace std;
@@ -83,7 +89,6 @@ void PrintDouble(const double* num, unsigned cnt) {
   if (FLAGS_bin) {
     write(STDOUT_FILENO, num, sizeof(*num) * cnt);
   } else {
-
     for (unsigned i = 0; i < cnt; ++i) {
       //FastInt64ToBuffer(inum[i], buf);
       DoubleToBuffer(num[i], buf);
@@ -103,6 +108,78 @@ void PrintBool(const uint8* num, unsigned cnt) {
     buf[0] = num[i] ? '1' : '0';
     puts(buf);
   }
+}
+
+using namespace boost;
+using fibers::channel_op_status;
+using util::FileIOManager;
+using puma::storage::SliceIndex;
+typedef std::unique_ptr<uint8_t[]> ReadBuf;
+
+struct Item {
+  strings::MutableByteRange buf;
+  FileIOManager::ReadResult read_result;
+  ReadBuf holder;
+
+  Item() {}
+  Item(Item&&) = default;
+  Item& operator=(Item&&) = default;
+};
+
+typedef fibers::buffered_channel<Item> ReadQueue;
+constexpr unsigned kCompletionQueueLen = 16;
+
+void ReadRequests(ReadQueue* q, unsigned* queue_len) {
+  channel_op_status st;
+  Item item;
+
+  unsigned reads = 0;
+  while(true) {
+    st = q->pop(item);
+    if (channel_op_status::closed == st)
+      break;
+    CHECK(st == channel_op_status::success) << int(st);
+    item.read_result.get();
+
+    --(*queue_len);
+    ++reads;
+    if (*queue_len < kCompletionQueueLen / 4)
+      boost::this_fiber::yield();
+  };
+  LOG(INFO) << "Processed " << reads << " reads";
+}
+
+void PrintAsync(const SliceIndex& si) {
+  int fd = open(FLAGS_table.c_str(), O_RDONLY, 0644);
+  CHECK_GT(fd, 0);
+
+  FileIOManager io_mgr(FLAGS_threads, FLAGS_io_len);
+
+  ReadQueue read_channel(kCompletionQueueLen);
+  size_t offset = 0;
+  unsigned queue_len = 0;
+  fibers::fiber read_fiber(fibers::launch::post, &ReadRequests, &read_channel, &queue_len);
+
+  for (int i = 0; i < si.slice_size(); ++i) {
+    const SliceIndex::Slice& slice = si.slice(i);
+    CHECK_GT(slice.len(), 0);
+
+    offset += slice.offset();
+    Item item;
+    item.holder.reset(new uint8_t[slice.len()]);
+    item.buf.reset(item.holder.get(), slice.len());
+
+    item.read_result = io_mgr.Read(fd, offset, item.buf);
+
+    channel_op_status st = read_channel.push(std::move(item));
+    CHECK(st == channel_op_status::success);
+    ++queue_len;
+
+    offset += slice.len();
+  }
+
+  read_channel.close();
+  read_fiber.join();
 }
 
 
@@ -217,8 +294,7 @@ void PrintStrings(VolumeIterator* str_it, VolumeIterator* sz_it) {
 
 }  // namespace
 
-
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
   MainInitGuard guard(&argc, &argv);
 
   CHECK(!FLAGS_table.empty()) << "Must be provided";
@@ -261,7 +337,11 @@ int main(int argc, char **argv) {
 
       PrintStrings(it.get(), str_len_it.get());
     } else {
-      PrintSlice(it.get());
+      if (FLAGS_threads > 0) {
+        PrintAsync(it->slice_index());
+      } else {
+        PrintSlice(it.get());
+      }
     }
   }
   return 0;
